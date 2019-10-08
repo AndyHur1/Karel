55a568b8e7f67f98cba9be70a8505a4054025c60
AndyHur1 <hurby@ssfs.org>
2019-10-08 11:56:15 -0400

    Finished Rectangle Class

--- src/UsingTheRectangleClass2.java
+++ src/UsingTheRectangleClass2.java
@@ -1,8 +1,16 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class UsingTheRectangleClass2 {
     public static void main(String[] args){
 
-        Scanner
+        Rectangle square = new Rectangle(100,100,50,50);
+        System.out.println(square);
+        square.grow(25,25);
+        System.out.println(square);
+        square.translate(25,25);
+        System.out.println(square);
+
+
     }
 }
