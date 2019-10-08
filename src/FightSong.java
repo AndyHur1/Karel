6a986f24846edb3dfc302821f572e3ad749f67a0
AndyHur1 <hurby@ssfs.org>
2019-10-08 12:06:44 -0400

    fightsong

---
+++ src/FightSong.java
@@ -0,0 +1,16 @@
+public class FightSong {
+
+
+        static void verse_1() {
+            System.out.println("Go, team, go!\nYou can do it.");
+            static void verse_2() {
+                System.out.println("You're the best,\nIn the West.");
+            static void black_space(){
+
+                }
+            }
+
+
+        }
+    }
+
